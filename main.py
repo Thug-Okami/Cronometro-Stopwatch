5a68186100376c3ee67f5266e6a52ad877773508
Thug-Okami <m.senna.cordeiro@gmail.com>
2024-09-01 19:12:52 -0300

    Alterações no Botão Iniciar

--- main.py
+++ main.py
@@ -8,17 +8,26 @@ cronometro_status = False
 
 def iniciar_cronometro():
     global cronometro_inicio, cronometro_status
-    cronometro_inicio = time.time()
-    cronometro_status = True
-    cronometro()
+    if cronometro_status == False:
+        cronometro_inicio = time.time()
+        cronometro_status = True
+        cronometro()
+    else:
+        cronometro_status = False
+        cronometro()
 
 def parar_cronometro():
-    global cronometro_status
+    global cronometro_status, cronometro_inicio
+    cronometro_inicio = time.time()
     cronometro_status = False
+    tempo_PassadoDisplay.set("0.00 segundos")
+    cronometro()
+    
 
 
 #Função Principal, atualiza o valor de tempo_PassadoDisplay
 def cronometro():
+    global tempo_Passado
     if cronometro_status:
         tempo_Passado = time.time() - cronometro_inicio
         tempo_PassadoDisplay.set(f"{tempo_Passado:.2f} segundos")
@@ -30,7 +39,6 @@ window = Tk()
 window.geometry("420x420")
 window.title("Cronometro")
 
-
 #Formatação do tempo_Passado para que possa ser lido pelo Tkinter
 tempo_PassadoDisplay = tk.StringVar()
 tempo_PassadoDisplay.set("0.00 segundos")
@@ -40,10 +48,10 @@ tempo_PassadoDisplay.set("0.00 segundos")
 label = ttk.Label(window, textvariable=tempo_PassadoDisplay, font=("Helvetica", 24))
 label.pack(pady=20)
 
-Botao_iniciar = ttk.Button(window, text="Iniciar", command=iniciar_cronometro)
+Botao_iniciar = ttk.Button(window, text="Iniciar/Pausar", command=iniciar_cronometro)
 Botao_iniciar.pack()
 
-Botao_iniciar = ttk.Button(window, text="Parar", command=parar_cronometro)
+Botao_iniciar = ttk.Button(window, text="Reset", command=parar_cronometro)
 Botao_iniciar.pack()
 
 
